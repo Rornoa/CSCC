a3e6e52eed2daa74ea8580fa07ac984f69278137
User <aaa>
2021-01-23 23:16:35 +0300

    Добавлен Клиент-Сервер без серриализации. Простой обмен строковыми сообщениями в однопоточном режиме.

--- CSCC/Server/src/server_kernel/Main.java
+++
@@ -1,9 +0,0 @@
-package server_kernel;
-
-public class Main {
-    public static void main(String[] args){
-    ApplicationManager applicationManager = new ApplicationManager();
-    applicationManager.initialize();
-
-    }
-}
